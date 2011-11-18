7ab0356e41b86115a1774e200eb4007e9f1772de
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 16:03:01 -0200

    Non-blocking.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,20 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
 
 	exec("find /", function (error, stdout, stderr) {
-		content = stdout;
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
 	});
-
-	return content;
 }
 
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called.");
-	return "Hello, upload.";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello, upload.");
+	response.end();
 }
 
 exports.start = start;
