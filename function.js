68dcb3f6dab3b52e1a9d5c07b597b6d646a4f507
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 14:01:29 -0200

    Passing functions around

---
+++ function.js
@@ -0,0 +1,11 @@
+// Passing functions around
+
+function say(word) {
+	console.log(word);
+}
+
+function execute(someFunction, value) {
+	someFunction(value);
+}
+
+execute(say, "Hello, world");
---
+++ server.js
@@ -0,0 +1,9 @@
+// A basic HTTP server
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello, world!");
+	response.end();
+}).listen(8888);
