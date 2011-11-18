489ab06cafb6a5b77b42a886495773142e6d1501
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 15:44:40 -0200

    Blocking.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,13 @@
 function start() {
 	console.log("Request handler 'start' was called.");
+
+	function sleep(milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	}
+
+	sleep(10000);
+
 	return "Hello, start.";
 }
 
