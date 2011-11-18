0aefc92dcc232c3136032a6b089c26a8a50f04eb
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 14:17:12 -0200

    Turning server.js into a module.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// A basic HTTP server
-
 var http = require("http");
 
-function onRequest(request, response) {
-	console.log("Request received.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello, world!");
-	response.end();
+function start() {
+	function onRequest(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello, world!");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started.");
+exports.start = start;
