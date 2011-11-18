c22578f3c3e8b4128d3df2917ea9b040a381af41
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 14:11:21 -0200

    Trying to figure out what a callback is.

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 var http = require("http");
 
 function onRequest(request, response) {
+	console.log("Request received.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello, world!");
 	response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
