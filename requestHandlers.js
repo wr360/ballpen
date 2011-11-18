e654a73d95e1891b4107a74fe8de873a50989280
Willian Rosa <willianrosa360@gmail.com>
2011-11-18 15:30:09 -0200

    Making the request handlers respond (how to not do it).

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
 	console.log("Request handler 'start' was called.");
+	return "Hello, start.";
 }
 
 function upload() {
 	console.log("Request handler 'upload' was called.");
+	return "Hello, upload.";
 }
 
 exports.start = start;
